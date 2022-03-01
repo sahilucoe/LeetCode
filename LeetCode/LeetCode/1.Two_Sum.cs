362bff39a7ba14d7e4be4aff3f954ddc5bf45783
Sahil Kumar <sahilucoe@hotmail.com>
2022-03-01 20:56:50 +0530

    1. Two Sum

---
+++ LeetCode/LeetCode/1.Two_Sum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace LeetCode
+{
+    public class TwoSum
+    {
+        public int[] Solve(int[] nums, int target)
+        {
+            // 1. Sort the input array
+            var input = nums.Select((x, i) => new { x, i }).OrderBy(y=>y.x).ToArray();
+
+
+            // 2. Iterate over the input array from both the ends
+            int start = 0, end = nums.Length - 1;
+            while (start < end)
+            {
+                if (input[start].x + input[end].x == target)
+                {
+                    break;
+                }
+                else if (input[start].x + input[end].x < target)
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+            return new[] { input[start].i, input[end].i };
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new TwoSum();
+            Console.WriteLine(t.Solve(new[] { 3, 2, 4 },6));
+        }
+    }
+}
