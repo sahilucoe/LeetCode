5466f66b1529eaf53a365b605c69446a87bc5c71
Sahil Kumar <sahilucoe@hotmail.com>
2022-03-06 22:55:45 +0530

    Fix

--- LeetCode/Problems/53.MaximumSubarray.cs
+++ LeetCode/Problems/53.MaximumSubarray.cs
@@ -12,7 +12,7 @@ namespace Problems
                 return nums[0];
             }
             var dp = new int[nums.Length];
-            dp[0] = Math.Max(0, nums[0]);
+            dp[0] = nums[0];
             for (int i = 1; i < nums.Length; i++)
             {
                 dp[i] = Math.Max(nums[i], nums[i]+dp[i-1]);
