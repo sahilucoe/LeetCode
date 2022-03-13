f719a5a89c8a153ce4d1e60b133e02a36d530178
Sahil Kumar <sahilucoe@hotmail.com>
2022-03-13 23:18:02 +0530

    1395

---
+++ LeetCode/Problems/1395.CountNumberOfTeams.cs
@@ -0,0 +1,52 @@
+﻿namespace Problems
+{
+    internal class CountNumberOfTeams
+    {
+        public int NumTeams(int[] rating)
+        {
+            int[] greaterOnRight = new int[rating.Length];
+            int[] smallerOnRight = new int[rating.Length];
+
+            greaterOnRight[rating.Length - 1] = 0;
+            smallerOnRight[rating.Length - 1] = 0;
+
+            for (var i = 0; i < rating.Length; i++)
+            {
+                for (var j = i; j < rating.Length; j++)
+                {
+                    if (rating[j] > rating[i])
+                    {
+                        greaterOnRight[i]++;
+                    }
+                    else if (rating[j] < rating[i])
+                    {
+                        smallerOnRight[i]++;
+                    }
+                }
+            }
+            var total = 0;
+
+            for (int i = 0; i < rating.Length; i++)
+            {
+                for (var j = i + 1; j < rating.Length; j++)
+                {
+                    if (rating[j] > rating[i])
+                    {
+                        total += greaterOnRight[j];
+                    }
+                }
+            }
+            for (int i = 0; i < rating.Length; i++)
+            {
+                for (var j = i + 1; j < rating.Length; j++)
+                {
+                    if (rating[j] < rating[i])
+                    {
+                        total += smallerOnRight[j];
+                    }
+                }
+            }
+            return total;
+        }
+    }
+}
--- LeetCode/Problems/Program.cs
+++ LeetCode/Problems/Program.cs
@@ -7,14 +7,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            int[][] dp = new int[3][];
-            dp[0] = new int[3] {1,1,1};
-            dp[1] = new int[3] {1,1,0};
-            dp[2] = new int[3] {1,0,1};
-
-            var f = new FloodFillSolution();
-            f.FloodFill(dp,1,1,2);
-
+            
         }
     }
 }
